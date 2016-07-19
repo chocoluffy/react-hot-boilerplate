ab16eabc1f640a73267c48a7920f55e947de2862
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-18 23:14:56 -0400

    Update app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  constructor(props){
+  	super(props);
+  	this.state={
+  		title: 'hello world'
+  	}
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <Text title={this.state.title} />
     );
   }
+};
+
+class Text extends Component {
+	constructor(props){
+	  super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<strong>{this.props.title}</strong>
+			</div>
+
+		);
+	}
 }
